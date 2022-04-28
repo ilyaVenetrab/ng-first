849d2aba50a688578145593addc2b81a797fae7d
ilabartenev <ilia.bartenev@novardis.com>
2022-04-28 19:04:38 +0300

    feat(work with pipes): added products-filter.pipe.ts added shared.module.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,48 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
+import { IProduct, products$ } from './data';
+import { UnSubscriber } from './unsubscribe';
+import { takeUntil } from 'rxjs/operators';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 
 @Component({
 	selector: 'ng-first-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent extends UnSubscriber implements OnInit {
 	public title = 'My App';
 
 	public text = 'Galaxy 10';
 
 	public drawer!: MatDrawer;
 
-	public changeText(text: string) {
+	public products: IProduct[] | null = null;
+
+	public products$ = products$;
+
+	public onlyFavorites: boolean = false;
+
+	public constructor(private _changeDetectorRef: ChangeDetectorRef) {
+		super();
+	}
+
+	public ngOnInit(): void {
+		this.products$.pipe(takeUntil(this.unSubscriber)).subscribe((p: IProduct[]) => {
+			this.products = p;
+		});
+	}
+
+	public changeText(text: string): void {
 		this.text = text;
 	}
 
-	public setDrawer(drawer: MatDrawer) {
+	public setDrawer(drawer: MatDrawer): void {
 		this.drawer = drawer;
+		this._changeDetectorRef.detectChanges();
+	}
+
+	public setFavourites(event: MatCheckboxChange): void {
+		this.onlyFavorites = event.checked;
 	}
 }
